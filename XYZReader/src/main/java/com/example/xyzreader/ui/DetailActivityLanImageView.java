dffbffb6192618660451bc719552cbe47ec4f939
Jianxun Gao <gao_jianxun@hotmail.com>
2016-04-12 14:27:50 +0800

    init
    initial one

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DetailActivityLanImageView.java
@@ -0,0 +1,23 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * from project Unsplash by nickbutcher
+ */
+public class DetailActivityLanImageView extends ImageView {
+
+    public DetailActivityLanImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int desiredHeight = width * 2 / 3;
+        super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(desiredHeight, MeasureSpec.EXACTLY));
+    }
+}
